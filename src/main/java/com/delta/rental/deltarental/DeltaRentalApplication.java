80312311c9f5f479d707fd4a546ae412a7ca6602
merve keser <mervekeser8888@gmail.com>
2023-12-11 14:01:42 +0300

    gitignore modified

---
+++ src/main/java/com/delta/rental/deltarental/DeltaRentalApplication.java
@@ -0,0 +1,13 @@
+package com.delta.rental.deltarental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delta/rental/deltarental/DeltaRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delta.rental.deltarental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
