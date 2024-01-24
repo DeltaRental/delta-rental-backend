ed40316913e3cb21f7ef549195e10661999a2e01
Onur Şenel <onrsenel@gmail.com>
2024-01-24 14:28:34 +0300

    Branch ve City Entity'si eklendi

---
+++ src/main/java/com/delta/rental/deltarental/entities/Branch.java
@@ -0,0 +1,42 @@
+package com.delta.rental.deltarental.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "branches")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Branch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "gsm")
+    private String gsm;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "manager_name")
+    private String managerName;
+
+    @Column(name = "post_code")
+    private String postCode;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+}
---
+++ src/main/java/com/delta/rental/deltarental/entities/City.java
@@ -0,0 +1,30 @@
+package com.delta.rental.deltarental.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "cities")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "city")
+    @JsonIgnore
+    private List<Branch> branches;
+}
