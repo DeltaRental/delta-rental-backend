c75e9cf8e64c6cf4cac78e25e6669bdfa50f54a5
Onur Şenel <onrsenel@gmail.com>
2024-02-24 21:29:40 +0300

    Araç siparişleri sıralama(order By)

--- src/main/java/com/delta/rental/deltarental/repositories/RentalRepository.java
+++ src/main/java/com/delta/rental/deltarental/repositories/RentalRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface RentalRepository extends JpaRepository<Rental,Integer> {
 
-    @Query(nativeQuery = true,value = "select r.* from rentals r inner join users u on r.user_id = u.id where u.id=?1")
+    @Query(nativeQuery = true,value = "select r.* from rentals r inner join users u on r.user_id = u.id where u.id=?1 order by r.start_date desc")
     List<Rental> filterRentalByUsers(int id);
 }
